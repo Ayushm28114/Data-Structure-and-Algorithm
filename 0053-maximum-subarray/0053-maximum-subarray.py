fbb5b554e7507ff29623d439573bf785e561f277
Ayush Mishra <ayushmishra28114@gmail.com>
2026-08-06 13:48:58 +0530

    Time: 40 ms (40.56%), Space: 31.5 MB (19.72%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,14 +1,10 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        if len(nums)==1 : return nums[0]
-        max_sum=nums[0]
-        curr_sum=nums[0]
-        i=1
+        max_sum=float('-inf')
+        curr_sum=float('-inf')
 
-
-        while i<len(nums):
-            curr_sum= max(nums[i], curr_sum + nums[i])
-            max_sum= max(curr_sum, max_sum)
-            i+=1
-
+        for num in nums:
+            curr_sum = max(curr_sum + num, num)
+            max_sum = max( max_sum, curr_sum)
+        
         return max_sum
