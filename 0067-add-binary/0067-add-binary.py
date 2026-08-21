808f958884b2d808d5ebd71d434b4b76a34732f9
Ayush Mishra <ayushmishra28114@gmail.com>
2026-08-21 15:32:50 +0530

    Time: 0 ms (100%), Space: 19.3 MB (51.43%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,9 +1,3 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        m=int(a,2)
-        n=int(b,2)
-
-        ans=m+n
-        
-        sol=format(ans,"b")
-        return sol
+        return format(int(a,2)+int(b,2),"b")
