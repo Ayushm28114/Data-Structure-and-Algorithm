dcc78ee63bd08bd7907e6e3fe15f95679c431450
Ayush Mishra <ayushmishra28114@gmail.com>
2026-09-07 11:27:04 +0530

    Time: 0 ms (100%), Space: 19.4 MB (26.31%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -2,13 +2,9 @@ class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         i=0
         j=0
-        a=""
-
         while i<len(s) and j<len(t):
             if s[i]==t[j]:
-                a+=t[j]
                 i+=1
             j+=1
-        
-        m="".join(a)
-        return m==s
+            
+        return i==len(s)
