c4114690b7fe9296574e04be14fa3c7574dce7aa
Ayush Mishra <ayushmishra28114@gmail.com>
2026-09-17 16:13:57 +0530

    Time: 374 ms (21.42%), Space: 21.5 MB (95.57%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.py
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.py
@@ -7,21 +7,14 @@ class Solution(object):
         """
         freq={}
         l, r = 0, 0
-        max_freq, curr_freq = 0, 0
+        max_freq = 0
 
         while r<len(nums):
-            freq[nums[r]] = freq.get(nums[r], 0) + 1
-            
-            if freq[nums[r]]<=k:
-                curr_freq+=1
-
-            else:
-                while freq[nums[r]]>k:
-                    freq[nums[l]]-=1
-                    l+=1
-                    curr_freq-=1
-                curr_freq+=1
-            max_freq = max(max_freq, curr_freq)
+            freq[nums[r]] = freq.get(nums[r], 0) + 1            
+            while freq[nums[r]]>k:
+                freq[nums[l]]-=1
+                l+=1
+            max_freq = max(max_freq, r-l+1)
             r+=1
         
         return max_freq
