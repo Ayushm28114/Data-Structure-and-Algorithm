7c59a6cc17af308df9810a09599224c240e55f92
Ayush Mishra <ayushmishra28114@gmail.com>
2026-09-21 11:47:29 +0530

    Time: 1 ms (49.84%), Space: 19.3 MB (92.9%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
@@ -6,13 +6,11 @@ class Solution:
                 nums[i]=nums[i]*2
                 nums[i+1]=0
         
-        l, r = 0, 0
+        l = 0
 
-        while r<len(nums):
-            if nums[r]==0:
-                r+=1
-            else:
+        for r in range(len(nums)):
+            if nums[r]!=0:
                 nums[r], nums[l] = nums[l], nums[r]
-                r+=1
                 l+=1
+
         return nums
