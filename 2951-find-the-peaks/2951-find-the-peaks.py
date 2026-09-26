10406498186d6b374d06e76997a8eda8a55e97ac
Ayush Mishra <ayushmishra28114@gmail.com>
2026-09-26 19:09:08 +0530

    Time: 3 ms (19.55%), Space: 19.2 MB (56.96%) - LeetHub

--- 2951-find-the-peaks/2951-find-the-peaks.py
+++ 2951-find-the-peaks/2951-find-the-peaks.py
@@ -2,10 +2,8 @@ class Solution:
     def findPeaks(self, nums: List[int]) -> List[int]:
         a=[]
         for i, num in enumerate(nums):
-            if i==0:
+            if i==0 or i==len(nums)-1:
                 continue
-            if i==len(nums)-1:
-                break
             
             if nums[i-1] < num > nums[i+1]:
                 a.append(i)
